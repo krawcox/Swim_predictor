201e7e2b6329b6398d16968e5f6ec156e28ce608
Pawel <Pawel217@live.missouristate.edu>
2022-10-30 10:19:18 -0500

    Added club and time stamps to not kill servers

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-
+import time
 import pandas as pd
 import numpy as np
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.swimrankings.net/index.php?page=athleteDetail&athleteId=4359097"
-
+#URL = "https://www.swimrankings.net/index.php?page=athleteDetail&athleteId=4359097"
+URL = "https://www.swimrankings.net/index.php?page=athleteDetail&athleteId=4844975"
 strona = requests.get(URL)
 
 soup = BeautifulSoup(strona.content, "html.parser")
@@ -14,10 +14,14 @@ wynik = soup.find_all("table", class_="athleteBest")
 
 krotki_basen = soup.find_all("tr", class_="athleteBest0")
 data_list = []
+count = 0
 for result in krotki_basen:
     event = result.find("td", class_="event")
     event_text = event.text
+    count += 1
+    print(count)
     link = event.find("a")
+    time.sleep(3)
     link = "https://www.swimrankings.net/index.php" + link['href']
     page = requests.get(link)
     zupka = BeautifulSoup(page.content, "html.parser")
@@ -25,30 +29,36 @@ for result in krotki_basen:
     for element in data:
         linijki = element.find_all("tr")
         pol = ""
+        num = 0
         for row in linijki:
             czasy = []
+            num += 1
+            print(num)
             if row["class"][0] == 'athleteRankingHead':
                 pol = row.text
             else:
                 czasy.append(event_text)
                 czasy.append(pol)
-                time = row.find("td", class_="time")
-                p = time.find("a")['href']
-                print(p)
-                redi = "https://www.swimrankings.net/index.php" + time.find("a")['href']
+                times = row.find("td", class_="time")
+                p = times.find("a")['href']
+                time.sleep(3)
+                redi = "https://www.swimrankings.net/index.php" + times.find("a")['href']
                 redi_page = requests.get(redi)
                 redi_soup = BeautifulSoup(redi_page.content, "html.parser")
-                club = redi_soup.find_all("td", class_="right")[1].text
-                print(club)
-                czasy.append(time.text)
+                club = 0
+                if redi_soup.find_all("td", class_="right") != []:
+                    club = redi_soup.find_all("td", class_="right")[1].text
+                czasy.append(times.text)
                 code = row.find("td", class_="code").text
                 czasy.append(code)
                 date = row.find("td", class_="date").text
                 czasy.append(date)
                 city = row.find("td", class_="city").text
                 czasy.append(city)
+                czasy.append(club)
                 data_list.append(czasy)
 df = pd.DataFrame(data_list)
+df.columns = ['event', 'pool', 'time', 'points', 'date', 'city', 'club']
 print(df.head())
 df.to_csv('athlete.csv')
 
